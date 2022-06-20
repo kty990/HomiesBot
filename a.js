8ac976686e42b0fdd71d25a1ec80cb2ed2aa75d4
kty990 <41134070+kty990@users.noreply.github.com>
2022-06-20 00:52:30 -0600

    Add /src/util/info files

---
+++ a.js
@@ -0,0 +1,22 @@
+console.log("Loading... a.js");
+module.exports.a = {
+    "apply" : ["a+"],
+    "play" : ["p", "pl"],
+    "skip" : ["s"],
+    "nowplaying" : ["np"],
+    "resume" : ["res", "r"],
+    "pause" : ["pp"],
+    "join" : ["enter", "fuckon", "waxon", "appear"],
+    "leave" : ["fuckoff", "waxoff", "disappear"],
+    "queue" : ["q"],
+    "say" : [],
+    "verify" : ["verifyme"],
+    "cmds" : ["commands"],
+    "aliases" : ["als"],
+    "help" : [],
+    "change" : ["alter","settings"],
+    "cah" : [],
+    "save" : []
+}
+
+console.log("a.js loaded... Success!");
---
+++ c.js
@@ -0,0 +1,23 @@
+console.log("Loading... c.js");
+module.exports.c = [
+    "apply",
+    "play (link)",
+    "skip",
+    "nowplaying",
+    "resume",
+    "pause",
+    "join",
+    "leave",
+    "queue",
+    "say (message)",
+    "verify",
+    "cmds",
+    "alliases",
+    "help",
+    "announcement (message) (channel) : Code Mentor Server only",
+    "change (setting) (new-value)",
+    "cah",
+    "save [dev-only]"
+]
+
+console.log("c.js loaded... Success!");
