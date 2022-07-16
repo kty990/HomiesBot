824ea69cdc7187a101d3866007decd31d2db708b
kty990 <41134070+kty990@users.noreply.github.com>
2022-07-15 20:50:42 -0600

    Updated cmdh.js

--- src/cmdh.js
+++ src/cmdh.js
@@ -11,6 +11,8 @@ const aliases = { // also edit the aliases in the aliases.js command module
     "pause": ["pp"],
     "join": ["enter", "fuckon", "waxon", "appear"],
     "leave": ["fuckoff", "waxoff", "disappear"],
+    "shuffle": [],
+    "loopqueue": [],
     "queue": ["q"],
     "say": [],
     "cmds": ["commands"],
@@ -39,17 +41,35 @@ function IsAliasOf(cmd) {
     return null;
 }
 
+function unregisterCommands(commands, callback) {
+    commands.fetch().then(collection => {
+        for (const [snowflake, cmd] of collection.entries()) {
+            console.log(`Attempting to unregister:\nSnowflake: ${snowflake}\t\tCmd: ${cmd}\n`);
+            commands?.delete(cmd.id)
+                .catch(console.error);
+        }
+        callback();
+        return;
+    })
+        .catch(err => {
+            console.error(err);
+            callback();
+            return;
+        });
+}
+
 class CommandHandler {
-    constructor(client) {
+    constructor(client, guild) {
         this.LoadedCommands = {};
         this.client = client;
+        this.guild = guild;
         this.subscription = null;
         this.voiceConnection = null;
 
         /**
          * Handle the bot being forceably removed from a voice channel by admin.
          */
-        let listener = client.on("voiceStateUpdate", (oldState, newState) => {
+        client.on("voiceStateUpdate", (oldState, newState) => {
             if (newState.member) {
                 if (newState.channel === null && newState.member.user === client.user) {
                     if (this.subscription) {
@@ -60,26 +80,96 @@ class CommandHandler {
                     }
                 }
             }
-        })
+        });
+
+        // client.on("interactionCreate", async interaction => {
+        //     if (!interaction.isCommand()) return;
+
+        //     try {
+        //         const member = interaction.member;
+        //         const author = interaction.user;
+        //         const channel = interaction.channel;
+        //         const guild = interaction.guild || channel.guild;
+        //         const { commandName, options } = interaction;
+
+        //         let replied = false;
+
+        //         await interaction.deferReply({
+        //             ephemeral: true,
+        //         }).catch(console.error);
+
+        //         let response;
+
+        //         if (this.LoadedCommands[commandName].isMusic) {
+        //             let musicData = { "subscription": this.subscription, "voice": this.voiceConnection };
+        //             response = await this.LoadedCommands[commandName].slashExe(musicData, interaction, interaction.client)
+        //                 .catch(console.error);
+        //             if (typeof response === "object") {
+        //                 this.subscription = response["subscription"];
+        //                 this.voiceConnection = response["voice"];
+        //             }
+        //         } else {
+        //             response = await this.LoadedCommands[commandName].slashExe(interaction, interaction.client)
+        //                 .catch(console.error);
+        //         }
+
+        //         if (typeof response === "string") {
+        //             if (!replied) {
+        //                 replied = true;
+        //                 interaction.editReply({
+        //                     content: response,
+        //                 })
+        //                     .catch(console.error);
+        //             }
+        //         }
+
+        //         if (!replied) {
+        //             replied = true;
+        //             interaction.editReply({
+        //                 content: `${commandName} used`,
+        //             })
+        //                 .catch(console.error);
+        //         }
+        //     } catch (err) {
+        //         embed(this.client, e => {
+        //             e.color = 0xeb4034;
+        //             e.description = `${err}`;
+        //             e.title = `Could not run ${commandName}`;
+        //             e.footer.text = "An error occured.";
+        //             message.channel.send({
+        //                 embeds: [e],
+        //             })
+        //                 .catch(console.error);
+        //         });
+        //         if (!replied) {
+        //             replied = true;
+        //             interaction.editReply({
+        //                 content: `An error occured. See error message output if provided for more information.`,
+        //             })
+        //                 .catch(console.error);
+        //         }
+        //     }
+        // });
     };
 
     /**
      * Initializes the CommandHandler with command data.
      * 
-     * @param {*} guildInfo
+     * @param {*} guildInfo Datastore instance
      * 
      * @returns void
      */
     Initialize(guildInfo) {
         this.guildInfo = guildInfo;
         let files = fs.readdirSync(__dirname + "/commands");
+
         files.forEach(file => {
             let filename = file.split('.');
             if (filename[1] == 'js') {
                 const { command } = require(`./commands/${filename[0]}.js`);
                 this.LoadedCommands[filename[0]] = new command(guildInfo);
             }
-        })
+        });
     }
 
     /**
@@ -127,7 +217,7 @@ class CommandHandler {
                         console.log(`Command debug:\nVoice: ${returnData['voice']}\nSubscription: ${returnData["subscription"]}\n`);
                     }
                 } else {
-                    command.exe(message, this.client, ...args);
+                    let result = command.exe(message, this.client, ...args);
                 }
                 if (command.shouldDelete) {
                     message.delete()
@@ -137,7 +227,11 @@ class CommandHandler {
                 console.warn(`An error occured trying to run ${cmd}: \t\t ${err}`);
                 embed(this.client, e => {
                     e.color = 0xeb4034;
-                    e.description = `${err}`;
+                    if (`${err}`.length >= 1025) {
+                        e.description = `Error message too long to send`;
+                    } else {
+                        e.description = `${err}`;
+                    }
                     e.title = `Could not run ${cmd.toLowerCase()}`;
                     e.footer.text = "An error occured.";
                     message.channel.send({
@@ -159,7 +253,7 @@ class CommandHandler {
                         console.log(`Alias debug:\nVoice: ${returnData['voice']}\nSubscription: ${returnData["subscription"]}\n`);
                     }
                 } else {
-                    command.exe(message, this.client, ...args);
+                    let result = command.exe(message, this.client, ...args);
                 }
                 if (command.shouldDelete) {
                     message.delete()
