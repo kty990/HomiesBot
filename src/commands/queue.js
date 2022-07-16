2c69509e55335d96a08c85be7b72ced79b02c4e1
kty990 <41134070+kty990@users.noreply.github.com>
2022-07-15 20:55:53 -0600

    Updated queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -67,15 +67,20 @@ class command {
         }
 
         let playing = null;
+        let totalDuration = 0;
         if (subscription !== null && subscription !== undefined) {
             let data = subscription.GetQueue();
             let queue = data['queue'];
             playing = data['playing'];
 
-            for (let i = (page - 1) * 10; i < queue.length; i++) {
-                if (i === page * 10) break;
+            for (let i = (page - 1) * 5; i < queue.length; i++) {
+                if (i === page * 5) break;
                 let track = queue[i];
-                DisplayQueue = DisplayQueue + `${i + 1}: [${track.title}](${track.url}) (${DisplayDuration(track.duration)}) : ${track.requester}`;
+                DisplayQueue = DisplayQueue + `${i + 1}: [${(track.title.length <= 50) ? track.title : track.title.substring(0, 49).replace("[", "").replace("]", "")}](${track.url}) (${DisplayDuration(track.duration)}) : ${track.requester}\n`;
+            }
+
+            for (let track in queue) {
+                totalDuration = totalDuration + track.duration;
             }
         }
 
@@ -83,9 +88,12 @@ class command {
             DisplayQueue = "[empty]";
         }
 
+
+
+
         embed(client, embed => {
             if (playing !== null) {
-                embed.description = `**Playing now**\n[${playing.title}](${playing.url}) (${DisplayDuration(playing.duration)}) : ${playing.requester}`;
+                embed.description = `**Playing now**\n[${(playing.title.length <= 50) ? playing.title : playing.title.substring(0, 49)}](${playing.url}) (${DisplayDuration(playing.duration)}) : ${playing.requester}`;
             } else {
                 embed.description = "**Playing now**\n[empty]";
             }
@@ -93,6 +101,11 @@ class command {
                 name: `Page ${page}`,
                 value: DisplayQueue,
                 inline: false,
+            });
+            embed.fields.push({
+                name: "\u2800",
+                value: `\`Queue length: ${DisplayDuration(totalDuration)}\``,
+                inline: false
             });
             embed.title = `${guild.name} Queue`;
             embed.footer.text = `Use ${this.guildInfo.Get('prefix')}queue [page] to view other pages of the queue`;
@@ -103,6 +116,74 @@ class command {
                     throw new Error(e);
                 });
         });
+        return {
+            "subscription": subscription,
+            "voice": voice,
+        }
+    }
+
+    /**
+     * 
+     * @param {*} interaction 
+     * @param {*} client 
+     * 
+     * @returns void
+     */
+    slashExe(musicData, interaction, client) {
+        return new Promise((resolve, reject) => {
+            let voice = musicData['voice'];
+            let subscription = musicData['subscription'];
+
+            const guild = interaction.guild;
+            const channel = interaction.channel;
+
+            let DisplayQueue = "";
+
+            const { options } = interaction;
+            let page = options.getInteger("page", false);
+            if (page === null || page === undefined) {
+                page = 1;
+            }
+            if (page <= 0) page = 1;
+
+            let playing = null;
+            if (subscription !== null && subscription !== undefined) {
+                let data = subscription.GetQueue();
+                let queue = data['queue'];
+                playing = data['playing'];
+
+                for (let i = (page - 1) * 5; i < queue.length; i++) { //10 changed to 5
+                    if (i === page * 5) break; //10 changed to 5
+                    let track = queue[i];
+                    DisplayQueue = DisplayQueue + `${i + 1}: [${(track.title.length <= 20) ? track.title : track.title.substring(0, 19)}](${track.url}) (${DisplayDuration(track.duration)}) : ${track.requester}`;
+                }
+            }
+
+            if (DisplayQueue.length <= 1) {
+                DisplayQueue = "[empty]";
+            }
+
+            embed(client, embed => {
+                if (playing !== null) {
+                    embed.description = `**Playing now**\n[${playing.title}](${playing.url}) (${DisplayDuration(playing.duration)}) : ${playing.requester}`;
+                } else {
+                    embed.description = "**Playing now**\n[empty]";
+                }
+                embed.fields.push({
+                    name: `Page ${page}`,
+                    value: DisplayQueue,
+                    inline: false,
+                });
+                embed.title = `${guild.name} Queue`;
+                embed.footer.text = `Use ${this.guildInfo.Get('prefix')}queue [page] to view other pages of the queue`;
+                channel.send({
+                    embeds: [embed],
+                })
+                    .catch(e => {
+                        reject(e);
+                    });
+            });
+        })
     }
 }
 
