b3027607413820cb540baa1b0a1636ba46f8d1a8
kty990 <41134070+kty990@users.noreply.github.com>
2022-07-15 20:55:13 -0600

    Updated ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,26 +1,54 @@
-const embed = require('../homiesEmbed.js');
+const { resolve } = require("dns");
 
-/**
- * 
- * @param {*} message 
- * @param {*} client 
- * @param {null} args 
- */
-function exe(message, client, args) {
-    const channel = message.channel;
-    let d = new Date();
-    let timeSinceEpoch = Math.round(Date.now());
-    let difference = Math.round(message.createdTimestamp - timeSinceEpoch) / 1000;
-    embed(client, embed => {
-        embed.description = `Pong: ${difference}s`;
-        embed.timestamp = null;
-        channel.send({
-            embeds: [embed]
-        }, d.toISOString())
+class command {
+    constructor(guildInfo) {
+        this.isMusic = false;
+        this.guildInfo = guildInfo;
+        this.shouldDelete = false;
+
+        this.name = "ping";
+        this.description = "Displays the latency the bot is experiencing.";
+        this.options = [];
+        // this.options = [
+        //     {
+        //         name: "url",
+        //         description: "A link to a public or unlisted YouTube video",
+        //         required: false,
+        //         type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+        //     }
+        // ];
+    }
+
+    /**
+     * 
+     * @param {*} message : Discord.js Message object
+     * @param {*} client : Client
+     * @param {null} args 
+    */
+    exe(message, client, args) {
+        const channel = message.channel;
+        let timeSinceEpoch = Math.round(Date.now());
+        let difference = Math.round(message.createdTimestamp - timeSinceEpoch);
+        message.reply(`Pong: ${Math.abs(difference)}ms`)
             .catch(console.error);
-    })
+    }
+
+    /**
+     * 
+     * @param {*} interaction 
+     * @param {*} client 
+     * 
+     * @returns void
+     */
+    slashExe(interaction, client) {
+        return new Promise((resolve, reject) => {
+            let timeSinceEpoch = Math.round(Date.now());
+            let difference = Math.round(message.createdTimestamp - timeSinceEpoch);
+            resolve(`Pong: ${Math.abs(difference)}ms`);
+        })
+    }
 }
 
 
 
-module.exports = { exe }
+module.exports = { command }
