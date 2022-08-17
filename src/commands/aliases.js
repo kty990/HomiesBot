568689e1b266ed44120d0929f3295e09996daa59
phxntxsmic <41134070+kty990@users.noreply.github.com>
2022-08-16 23:30:30 -0600

    2022-08-16 Update

--- src/commands/aliases.js
+++ src/commands/aliases.js
@@ -1,5 +1,4 @@
 const embed = require('../homiesEmbed.js');
-const Discord = require('discord.js');
 
 const aliases = {
     "play": ["p", "pl"],
@@ -11,6 +10,7 @@ const aliases = {
     "leave": ["fuckoff", "waxoff", "disappear"],
     "queue": ["q"],
     "say": [],
+    "purge": [],
     "cmds": ["commands"],
     "aliases": ["als"],
     "help": [],
@@ -20,7 +20,11 @@ const aliases = {
     "settings": ["alter", "change"],
     "cah": [],
     "password": ["pw"],
+    "bugs": ["knownbugs", "bug"],
     "uno": [],
+    "coup": [],
+    "premove": [],
+    "gtw": ["guesstheword"],
 };
 
 
@@ -48,26 +52,40 @@ class command {
      * @param {*} client : Client
      * @param {null} args 
     */
-    exe(message, client, page) {
+    async exe(message, client, page) {
         if (page === null || page === undefined) {
             page = 1;
         }
+        if (page * 10 > Object.entries(aliases).length) {
+            embed(client, myEmbed => {
+                myEmbed.title = `Aliases Page ${page}`;
+                myEmbed.description = "[empty]";
+                myEmbed.footer.text = `Say ${this.guildInfo.Get('prefix')}aliases [page number]`;
+                message.channel.send({
+                    embeds: [myEmbed],
+                })
+                    .catch(console.error);
+
+            });
+            return;
+        }
         if (page <= 0) page = 1;
-        var count = 0;
+        var count = 0;//(page == 1) ? 0 : (page - 1) * 10;
         var alliases = "";
         for (const [command, allias] of Object.entries(aliases)) {
 
+
             // Choice 1
-            if (allias == null || allias.length == 0) {
+            if (allias == null || allias.length == 0 || aliases == undefined) {
                 alliases = alliases + `${this.guildInfo.Get('prefix')}${command} : **n/a**\n`;
                 continue;
             }
             // Choice 2 is to put [empty] in place of the allias(es)
             count++;
-            if (count > (page - 1) * 10 && count < page * 10) {
+            if (count > ((page - 1) * 10) && count < (page * 10)) {
                 var newallias = `${this.guildInfo.Get('prefix')}${command} : \`${this.guildInfo.Get('prefix')}${allias.join(", " + this.guildInfo.Get('prefix'))}\``;
                 alliases = alliases + newallias + "\n";
-            } else if (count > page * 10) {
+            } else if (count >= (page * 10)) {
                 break;
             }
         }
@@ -80,12 +98,13 @@ class command {
 
 
         embed(client, myEmbed => {
-            myEmbed.title = "Aliases";
+            myEmbed.title = `Aliases Page ${page}`;
             myEmbed.description = alliases;
             myEmbed.footer.text = `Say ${this.guildInfo.Get('prefix')}aliases [page number]`;
             message.channel.send({
                 embeds: [myEmbed],
-            }, d.toISOString());
+            })
+                .catch(console.error);
         });
     }
 
