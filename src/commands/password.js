64d5d65d1256148fadbdbe96cb83f2d0edf84316
phxntxsmic <41134070+kty990@users.noreply.github.com>
2022-10-29 17:21:26 -0600

    2022-10-29 Update

--- src/commands/password.js
+++ src/commands/password.js
@@ -10,6 +10,7 @@ class command {
         this.name = "password";
         this.description = "DMs a randomly generated password.";
         this.options = [];
+        this.aliases = ["pw"];
     }
 
     /**
@@ -18,7 +19,7 @@ class command {
      * @param {*} client : Client
      * @param {null} args 
     */
-    exe(message, client, ...args) {
+    async exe(message, client, ...args) {
         const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '!', '@', '#', '$', '%', '^', '&', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
