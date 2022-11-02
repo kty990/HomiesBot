84b2387a544cbbf3f6cc2a06f7e2b85ab7b16f33
phxntxsmic <41134070+kty990@users.noreply.github.com>
2022-11-01 23:10:40 -0600

    Update test.js

--- src/commands/developer/test.js
+++ src/commands/developer/test.js
@@ -1,13 +1,10 @@
 const embed = require('../../homiesEmbed.js');
-
-const vm = require('vm');
+const vm2 = require('vm2');
 
 class whitelist {
     constructor() {
         // Stored using the user's ID.
-        this.allowed = [
-            "177486429780312064"
-        ];
+        this.allowed = require('./whitelist.json')['whitelist'];
     }
 
     IsWhitelisted(user_id) {
@@ -44,30 +41,106 @@ class command {
         if (!this.allowed.IsWhitelisted(message.author.id)) {
             throw new Error("This is a secret command. Only whitelisted developers and moderators have access to this command.");
         }
-        const content = message.content;
+        let content = message.content.split(" ");
         /**
          * TODO:
-         * Do check in content for illegal/unaccepted code
-         * Run code -> Provide truncated (if necessary) output to user
+         * Do check in content for illegal/unaccepted code                    INCOMPLETE
+         * Run code -> Provide truncated (if necessary) output to user        INCOMPLETE
          * 
          * Input types for code:
-         *      - message.content
-         *      - message attachments if filetype in [.txt]
+         *      - message.content                                             COMPLETED
+         *      - message attachments if filetype in [.txt]                   INCOMPLETE
          */
 
-        throw new Error("UnimplementedCommandError: test.js");
-    }
+        content.splice(0, 1); // removes command from content
 
+        // Do check for unaccepted code
+
+        let result = []; // change to list
+        let vm = new vm2.VM({ console: 'inherit', sandbox: { result } });
+
+        // Run code
+        vm.run(`console.log = function(value) {
+            result.push(value);
+        };
+
+        ` + content.join(" "), 'vm.js');
+
+        // try {
+        //     vm.run(`console.log = function(value) {
+        //         result.push(value);
+        //     };
+
+        //     ` + content.join(" ")).catch(console.error);
+        // } catch (err) {
+        //     let truncated = "";
+        //     if (`${err}`.length > 1000) {
+        //         truncated = "\n(...)";
+        //     }
+        //     embed(client, e => {
+        //         e.color = 0xeb4034;
+        //         e.description = `${`${err}`.substring(0, 1000)}${truncated}`;
+        //         e.title = "Syntax Error";
+        //         e.footer.text = `Command run by ${message.author.tag}`;
+        //     })
+        // }
+
+
+        console.log(`result: ${result}`);
+
+        embed(client, (myEmbed) => {
+            myEmbed.title = `Result of ${message.author.tag}'s ${this.guildInfo.Get('prefix')}test`;
+
+            // Truncate if necessary : 1024 char max per field value
+            let remaining = result.join("\n");
+            let addedChars = 0;
+            while (remaining.length > 0) {
+                if (remaining.length > 1024) {
+                    myEmbed.fields.push(
+                        {
+                            name: "\u2800",
+                            value: `${remaining.substring(0, 1024)}`,
+                            inline: false,
+                        }
+                    );
+                    remaining = remaining.substring(1024);
+                    addedChars += 1024;
+                    if (addedChars > (1024 * 4)) {
+                        myEmbed.fields.push(
+                            {
+                                name: "\u2800",
+                                value: `(...) omitted the rest of the output, exceeded ${1024 * 4} characters`,
+                                inline: false,
+                            }
+                        );
+                        break;
+                    }
+                } else {
+                    myEmbed.fields.push(
+                        {
+                            name: "\u2800",
+                            value: `${remaining}`,
+                            inline: false,
+                        }
+                    );
+                    break;
+                }
+            }
+            message.channel.send({ embeds: [myEmbed] })
+                .catch(console.error);
+        });
+
+
+    }
     /**
-     * 
-     * @param {*} interaction 
-     * @param {*} client 
-     * 
-     * @returns void
-     */
+    * @param {*} musicData
+    * @param {*} interaction 
+    * @param {*} client 
+    * 
+    * @returns void
+    */
     slashExe(musicData, interaction, client) { }
 }
-
 
 
 module.exports = { command }
